2f310f5f6465e23944e8c7806ef1269c23069dab
daisycharia <daisymacharia@gmail.com>
2020-10-15 01:09:39 +0300

    add signout

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,5 @@
 import pkg from "express";
-import { login, register, getUser } from "./services/users.js";
+import { login, register, getUser, signout } from "./services/users.js";
 
 const { Router } = pkg;
 const router = Router();
@@ -7,5 +7,6 @@ const router = Router();
 router.post("/register", register);
 router.post("/login", login);
 router.get("/user", getUser);
+router.get("/logout", signout);
 
 export default router;
